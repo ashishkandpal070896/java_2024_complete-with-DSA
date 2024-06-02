bbfdb2304fab495672720a94bb6269940941fa6b
ashishkandpal070896 <147151225+ashishkandpal070896@users.noreply.github.com>
2024-06-02 18:01:28 +0000

    main

---
+++ javabasic_01/Main01.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Main01{
+   
+        public static void main(String[] args) {
+            ArrayList<Integer> list1 = new ArrayList<>();
+            ArrayList<String> list2 = new ArrayList<>();
+            ArrayList<Boolean> list3 = new ArrayList<>();
+            
+            list1.add(1);
+            list1.add(2);
+            list1.add(3);
+            list1.add(4);
+            list1.add(5);
+      
+
+        System.out.println(list1);
+    }
+}
