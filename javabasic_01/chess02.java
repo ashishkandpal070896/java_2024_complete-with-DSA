b231f775ab52e021eec215ae324cdd3945fd65a0
ashishkandpal070896 <147151225+ashishkandpal070896@users.noreply.github.com>
2024-06-09 10:42:30 +0000

    board

---
+++ javabasic_01/chess02.java
@@ -0,0 +1,60 @@
+public class chess02 {
+    public static boolean isSafe(char board[][], int row, int col) {
+        // Check vertical up
+        for (int i = row - 1; i >= 0; i--) {
+            if (board[i][col] == 'Q') {
+                return false;
+            }
+        }
+        // Check left diagonal up
+        for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
+            if (board[i][j] == 'Q') {
+                return false;
+            }
+        }
+        // Check right diagonal up
+        for (int i = row - 1, j = col + 1; i >= 0 && j < board.length; i--, j++) {
+            if (board[i][j] == 'Q') {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    // Queen problem in backtracking
+    public static void nQueens(char board[][], int row) {
+        if (row == board.length) {
+            printBoard(board);
+            return;
+        }
+        // Column
+        for (int j = 0; j < board.length; j++) {
+            if (isSafe(board, row, j)) {
+                board[row][j] = 'Q';
+                nQueens(board, row + 1);
+                board[row][j] = 'x';
+            }
+        }
+    }
+
+    public static void printBoard(char board[][]) {
+        System.out.println(" ----- chess board ----- ");
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board.length; j++) {
+                System.out.print(board[i][j] + "  ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        int n = 4;
+        char board[][] = new char[n][n];
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board.length; j++) {
+                board[i][j] = 'x';
+            }
+        }
+        nQueens(board, 0);
+    }
+}
